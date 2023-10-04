5782e83569b987df98f25592c0c002ccdb106b16
Sergey Ertushov <ertushov.sergey@email.com>
2023-10-04 16:01:57 +0300

    создал переменные в файле js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const myFirst = 1
 
-const myFunc = () => {
-    console.log(myFirst);
-}
+let title = "Главный заголовок"
+let screens = "1024x740"
+let screenPrice = "700$"
+let rollback = "че то типо откатиться назад"
+let fullPrice = "Весь список"
+let adaptive = "Варианты экрана"
+
+alert(title); s
+console.log(myFirst);
+S
